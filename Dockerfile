d60ded39c5e6cda9ebdebefd4e49c4f43f6c8862
Thomas CAEN <42998330+TCaen@users.noreply.github.com>
2024-03-14 15:50:13 +0100

    Revert "config mongo/express"

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,20 @@
+
+
 FROM node:18
 
-
+# Create app directory
 WORKDIR /usr/src/app
 
-COPY ./package*.json ./
+# Install app dependencies
+# A wildcard is used to ensure both package.json AND package-lock.json are copied
+COPY package*.json ./
 
 RUN npm install
+# If you are building your code for production
+# RUN npm ci --only=production
 
+# Bundle app source
 COPY . .
 
-
-
 EXPOSE 8080
-CMD [ "node", "api/app.js" ]
+CMD [ "node", "server.js" ]
--- src/api/app.js
+++ src/api/app.js
@@ -1,66 +1,14 @@
+// server.js
+
 const express = require('express');
-const mongoose = require('mongoose');
-const articleRoutes = require('./routes/articleRoutes')
-const helmet = require('helmet');
 
-require('dotenv').config();
-
-require('./config/db');
-
+//Create an app
 const app = express();
-
-
-app.use(helmet());
-
-// // Configuration de la connexion à MongoDB
-// mongoose.connect('mongodb://mongo:27017/articleapi', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
-
-
-const mongoDBURL = 'mongodb://mongo:27017/articleapi';
-
-mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', function() {
-  console.log('Connected to MongoDB');
+app.get('/', (req, res) => {
+    res.send('Hello world\n');
 });
 
-
-
-// 
-// mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-
-app.use(express.json());
-
-app.use('/articles', articleRoutes);
-
-
-app.use((req, res, next) => {
-    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:");
-    next();
-});
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-// 
-// app.get('/', (req, res) => {
-//     res.send('Hello world\n');
-// });
-
-// const PORT = process.env.PORT || 8080;
-
-// app.listen(PORT, () => {
-//     console.log(`Running on port ${PORT}`);
-// });
-
-
-
-
+//Listen port
+const PORT = 8080;
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
