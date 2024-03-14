99b033d1b39258db442651b4cd8845ff04bf27ea
HEITZMANNM <mheitzmann1@hotmail.fr>
2024-03-14 13:06:47 +0100

    config mongo/express

--- src/api/app.js
+++ src/api/app.js
@@ -1,14 +1,29 @@
-// server.js
-
 const express = require('express');
+const helmet = require('helmet');
 
-//Create an app
+require('dotenv').config();
+
+require('./config/db');
+
 const app = express();
+
+// Utilisation du middleware Helmet pour la sécurité
+app.use(helmet());
+
+// Configuration de la CSP pour autoriser les icônes favicon depuis le même domaine
+app.use((req, res, next) => {
+    res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:");
+    next();
+});
+
+// Route pour tester l'environnement
 app.get('/', (req, res) => {
     res.send('Hello world\n');
 });
 
-//Listen port
-const PORT = 8080;
-app.listen(PORT);
-console.log(`Running on port ${PORT}`);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
+
---
+++ src/api/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const mongoDB_Url = process.env.MONGODB_URL;
+// const mongoDB_Url = "mongodb://localhost:27017/articleapi";
+
+mongoose.connect(mongoDB_Url, {useNewUrlParser:true, useUnifiedTopology:true});
+
+mongoose.connection.on('connected', res=>console.log('connected'));
+
+mongoose.connection.on('error', err=>console.log('err'));
+
+
+
+
+
+
+
