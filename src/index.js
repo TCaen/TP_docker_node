af9353929ef14eab27d9b7cfae73835b0b629785
Neycha <regentmarysapro@gmail.com>
2024-03-15 10:33:17 +0100

    Decommente index.hjs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import app from "./app.js";
-// import config from "../config.js";
+import app from "./app.js";
+import config from "../config.js";
 
-// app.listen(3000, () => {
-//     console.log(config.app_name+" Started on Port 3000");
-// });
+app.listen(3000, () => {
+    console.log(config.app_name+" Started on Port 3000");
+});
