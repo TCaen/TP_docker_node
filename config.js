4de27d1ee419326106f94f3b9c690f5ea8925876
Neycha <regentmarysapro@gmail.com>
2024-03-15 10:38:21 +0100

    Decommente config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// const config = {
-//     app_name: process.env['APP_NAME'],
-//     port: process.env['PORT'] ?? 3000,
-//     db_uri: process.env['DB_URI'] ?? 'mongodb://localhost:27017/docker',
-//     db_options: {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     }
-// }
+const config = {
+    app_name: process.env['APP_NAME'],
+    port: process.env['PORT'] ?? 3000,
+    db_uri: process.env['DB_URI'] ?? 'mongodb://localhost:27017/docker',
+    db_options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+}
 
-// export default config;
+export default config;
